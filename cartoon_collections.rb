8fefcaa5248cb3087ac9de615aae66c04335c923
droll-accelerator-1483 <srsani@gmail.com>
2019-07-24 15:11:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,9 +15,17 @@ def summon_captain_planet (array_in)
 end
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array_in)
+  output = false
+  array_in.each do |element|
+    if element.length >=5
+      output = true
+    end
+  end
+  return output
 end
+
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
